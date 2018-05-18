2157718d4cb58f7c7252cf760eba6b581afacdba
cuongvomanh <cuong.vomanh195@gmail.com>
2018-05-18 14:25:35 +0700

    change_test_file

--- image/mnist/test.py
+++ image/mnist/test.py
@@ -2,9 +2,12 @@ from matplotlib import pyplot as plt
 from random import randint
 import mnist
 import tensorflow as tf 
-saver = tf.train.Saver()
+sess = tf.Session()
+saver = tf.train.import_meta_graph('my-model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+# saver = tf.train.Saver()
 with tf.Session() as sess:
-    saver.restore(sess, "./my-model")
+    saver.restore(sess, "./my-model.meta")
 num = randint(0, mnist.test.images.shape[0])
 img = mnist.test.images[num]
 
