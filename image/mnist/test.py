4dac0800af9b1e320b2bd574657bd133c2348515
cuongvomanh <cuong.vomanh195@gmail.com>
2018-05-18 15:59:46 +0700

    add_get_mnist_file

--- image/mnist/test.py
+++ image/mnist/test.py
@@ -1,6 +1,24 @@
 from matplotlib import pyplot as plt
 from random import randint
-import mnist
+import random
+# from mnist import MNIST
+import get_mnist
+import cv2
+import numpy as np
+train_images, train_labels, test_images, test_labels = get_mnist.mnist('data/')
+# mndata = MNIST('data')
+# images, labels = mndata.load_training()
+# print(images)
+# num = randint(0, mndata.test.images.shape[0])
+# img = mndata.test.images[num]
+# img = images[0]
+# index = random.randrange(0, len(test_images))  # choose an index ;-)
+# print(mndata.display(test_images[index]))
+img = test_images[0]
+print(type(img))
+# img =np.array(img)
+
+cv2.imshow('img', img)
 import tensorflow as tf 
 sess = tf.Session()
 saver = tf.train.import_meta_graph('my-model.meta')
@@ -8,8 +26,8 @@ saver.restore(sess, tf.train.latest_checkpoint('./'))
 # saver = tf.train.Saver()
 with tf.Session() as sess:
     saver.restore(sess, "./my-model.meta")
-num = randint(0, mnist.test.images.shape[0])
-img = mnist.test.images[num]
+# num = randint(0, mnist.test.images.shape[0])
+# img = mnist.test.images[num]
 
 classification = sess.run(tf.argmax(y, 1), feed_dict={x: [img]})
 plt.imshow(img.reshape(28, 28), cmap=plt.cm.binary)
